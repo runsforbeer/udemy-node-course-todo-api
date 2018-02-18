ef57dd742ebfe52cbc1a88a4292fd91038c4941a
Don Marsh <don.marsh@gmail.com>
2018-02-17 21:05:07 -0500

    seed data to test create user; add tests for users

---
+++ server/tests/seed/seed.js
@@ -0,0 +1,56 @@
+const {ObjectID} = require('mongodb');
+const {Todo} = require('./../../models/todo');
+const {User} = require('./../../models/user');
+const jwt = require('jsonwebtoken');
+
+const todos = [{
+    _id: new ObjectID(),
+    text: 'First test todo'
+}, {
+    _id: new ObjectID(),
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 333
+}
+];
+
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+
+const users = [{
+    _id: userOneId,
+    email: 'don@example.com',
+    password: 'userOnePass',
+    tokens: [{
+        access: 'auth',
+        token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString() // abc123 is same secret in tests
+    }]
+}, {
+    _id: userTwoId,
+    email: 'don1@example.com',
+    password: 'userTwoPass'
+}]
+
+const populateTodos = (done) => {
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done()); // will remove all todos
+};
+
+// need to customize this so pwd is hashed like in middleware
+const populateUsers = (done) => {
+    User.remove({}).then(() => {
+        var userOne = new User(users[0]).save();
+        var userTwo = new User(users[1]).save();
+
+        // waith for both promises above
+        return Promise.all([userOne, userTwo]);
+    }).then(() => done());
+};
+
+module.exports = {
+    todos,
+    users,
+    populateTodos,
+    populateUsers
+};
